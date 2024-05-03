b79cd65b160889c9e99732f205d99187bfff5396
earthgaro1202 <earthwora47@gmail.com>
2024-05-03 13:14:33 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,315 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "ทางเข้าหน้าสำนักวิทยบริการ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.05488446030306271,
+        "pitch": -0.09116000676517366,
+        "fov": 1.4004924010941646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.03998553224583645,
+          "pitch": 0.08977188585377505,
+          "rotation": 0,
+          "target": "1--1"
+        },
+        {
+          "yaw": -0.5771542594834749,
+          "pitch": 0.11203815820823948,
+          "rotation": 5.497787143782138,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5634256163759144,
+          "pitch": -0.28289289304352394,
+          "title": "อาคารข้อมูลท้องถิ่น",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">ชั้น 2&nbsp;</font></font><div>Co-working space</div><div>ชั้น 3</div><div>Learning space</div><div>Together space</div>"
+        }
+      ]
+    },
+    {
+      "id": "1--1",
+      "name": "ลานทางเดิน 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.5226457621568414,
+        "pitch": 0.11625748801018787,
+        "fov": 1.4004924010941646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.686866882192131,
+          "pitch": 0.131780756080925,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.44986676604052533,
+          "pitch": 0.13335453476549652,
+          "rotation": 0,
+          "target": "2--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--2",
+      "name": "ลานทางเดิน 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.2941506110273373,
+        "pitch": 0.09269080358117598,
+        "fov": 1.4004924010941646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9295950883033228,
+          "pitch": 0.09284032141982834,
+          "rotation": 0,
+          "target": "1--1"
+        },
+        {
+          "yaw": 0.7312510692959275,
+          "pitch": 0.10590689325198355,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": 2.2540625294949574,
+          "pitch": 0.08374149668843955,
+          "rotation": 0,
+          "target": "4--3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5178784273195465,
+          "pitch": 0.11048909881333557,
+          "title": "ห้องสมุดในสวน",
+          "text": "พื้นที่ให้บริการห้องสมุดในสวน"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "สวนเรืองแสง",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.539780761738486,
+        "pitch": -0.06420549438116652,
+        "fov": 1.4004924010941646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7979280232860155,
+          "pitch": 0.1781359905921427,
+          "rotation": 0,
+          "target": "2--2"
+        },
+        {
+          "yaw": 0.7113152179775355,
+          "pitch": 0.06045374392806302,
+          "rotation": 5.497787143782138,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.954179099021907,
+          "pitch": -0.009221617232721968,
+          "title": "UBU Maker space",
+          "text": "พื้นที่ให้บริการอุปกรณ์ เลเซอร์ คัท , เครื่องตัดสติ๊กเกอร์, เครื่องพิมพ์สามมิติ และ เครื่องพ่นสี"
+        },
+        {
+          "yaw": -3.1316324195735366,
+          "pitch": 0.03598010104250804,
+          "title": "พื้นที่สวนเรืองแสง",
+          "text": "พื้นที่ให้บริการสวนเรืองแสง"
+        }
+      ]
+    },
+    {
+      "id": "4--3",
+      "name": "ลานทางเดิน 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 3.0192437265302345,
+        "pitch": 0.18694778914076693,
+        "fov": 1.4004924010941646
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.9262668624064663,
+          "pitch": 0.21932342792167958,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.10828284387575238,
+          "pitch": 0.19554998462532147,
+          "rotation": 0,
+          "target": "2--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "หน้าห้องสมุด",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.14748785827775102,
+        "pitch": 0.22542027911075735,
+        "fov": 1.4004924010941646
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2936672701371332,
+          "pitch": 0.2468311487769519,
+          "rotation": 0,
+          "target": "4--3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.15400714907909752,
+          "pitch": 0.01751308962189313,
+          "title": "อาคารข้อมูลสารสนเทศ",
+          "text": "ชั้น 1<div>Edutenment Zone</div><div>ห้องอ่านนิยายเรื่องสั้น</div><div>มุมอินเดีย</div><div>ชั้น 2</div><div>ห้องประชุมกลุ่มย่อย</div><div>ศูนย์เครื่องมือวิจัย</div><div>ห้องประชุม</div><div>ชั้น 3</div><div>ห้องโสต (ห้องดูภาพยนต์)</div><div>พื้นที่อ่านเดี่ยว</div>"
+        },
+        {
+          "yaw": -3.1008643300193732,
+          "pitch": 0.10565813500405596,
+          "title": "ห้องสมุดในสวน",
+          "text": "พื้นที่ให้บริการห้องสมุดในสวน"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
